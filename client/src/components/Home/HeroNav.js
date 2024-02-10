487d5bf3a819aed1f646971f4a017beac1ac6dea
Om Sharma <omsharma1813111032@gmail.com>
2024-02-10 13:41:47 +0530

    Hero Nav Updated ( Responsiveness Added )

--- client/src/components/Home/HeroNav.js
+++ client/src/components/Home/HeroNav.js
@@ -1,12 +1,34 @@
-import {Box, styled, Typography} from "@mui/material"
+import {Box, styled, Typography, createTheme, ThemeProvider} from "@mui/material"
 import Carousel from "react-multi-carousel"
 import { navData } from "../../constants/data";
 
 
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xxs: 0, // small phone
+      xs: 300, // phone
+      sm: 600, // tablets
+      md: 900, // small laptop
+      lg: 1200, // desktop
+      xl: 1536 // large screens
+    }
+  }
+});
 
-const Components = styled(Box)`
-    margin: 5px 60px 5px 60px;
-`
+
+
+const HeroNavData = {
+  margin:{
+    xxs:0,
+    sx:0,
+    sm:0,
+    md:'5px 60px 5px 60px',     
+    lg:'5px 60px 5px 60px',     
+    xl:'5px 60px 5px 60px',     
+
+  }
+}
 
 const Container = styled(Box)`
     padding:12px 8px;
@@ -28,41 +50,42 @@ const HeroNav = () => {
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
           items: 9,
-          slidesToSlide: 3 // optional, default to 1.
+          slidesToSlide: 1 // optional, default to 1.
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
-          items: 5,
-          slidesToSlide: 2 // optional, default to 1.
+          items: 4,
+          slidesToSlide: 1 // optional, default to 1.
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
-          items: 3,
+          items: 4,
           slidesToSlide: 1 // optional, default to 1.
         }
       };
 
 
   return (
-    <Components>
-        <Carousel
-            responsive={responsive}
-            autoPlay={false}
-            removeArrowOnDeviceType={["desktop","tablet", "mobile"]}
-        >
-        
+    <ThemeProvider theme={theme} >
+      <Box sx={HeroNavData} >
+          <Carousel
+              responsive={responsive}
+              autoPlay={false}
+              removeArrowOnDeviceType={["desktop","tablet", "mobile"]}
+          >
 
-        {
-            navData.map(data=>(
-                <Container>
-                    <img src={data.url} alt={data.text} style={{width:"64px"}} />
-                    <Texted>{data.text}</Texted>
-                </Container>
-            ))
-        }
-        
-        </Carousel>        
-    </Components>
+          {
+              navData.map(data=>(
+                  <Container>
+                      <img src={data.url} alt={data.text} style={{width:"64px"}} />
+                      <Texted>{data.text}</Texted>
+                  </Container>
+              ))
+          }
+          
+          </Carousel>        
+      </Box>
+    </ThemeProvider>
   )
 }
 
