d4ce58e28f5492a694e137788e1fbe82b509f643
Om Sharma <omsharma1813111032@gmail.com>
2024-02-10 13:42:06 +0530

    Home Page Banner Component Added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,21 @@
-import {Box} from "@mui/material"
-// import HeroNav from "./HeroNav"
+import {Box, styled} from "@mui/material"
 import HeroNav from "./HeroNav"
+import Banner from "./Banner"
+
+const BannerStyle = styled(Box)`
+    padding:10px;
+    background:#f2f2f2;
+`
 
 const Home = () => {
   return (
 
-    // <Box style={{marginTop:'54px',padding:'10px 0 10px 0',backgroundColor:"lightgrey"}}>
     <Box style={{marginTop:'54px',padding:'10px 0 10px 0'}}>
-        {/* <HeroNav/> */}
-        <HeroNav />
 
+        <HeroNav/>
+        <BannerStyle>
+            <Banner/>
+        </BannerStyle>
     </Box>
   )
 }
